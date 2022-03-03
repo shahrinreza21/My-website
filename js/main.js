0292244324c76e05a674d27ebecd7b8a20f80e83
shahrinreza21 <shahrinreza13172000@gmail.com>
2022-03-02 22:03:13 -0800

    this is my site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#example", {
+    strings:["Designer", "Developer"],
+    typeSpeed: 200,
+    backspaceSpeed: 20,
+    backspaceDelay: 800,
+    repeatDelay: 1000,
+    repeat: true,
+    loop: true,
+    autoStart: true,
+    startDelay: 100,
+  });
